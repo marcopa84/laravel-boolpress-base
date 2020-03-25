26aeb82ec73594d0886bf9c03bfd3d0b779461b0
Marco <marcopa84@hotmail.it>
2020-03-25 18:01:13 +0100

    bug fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,7 +45,7 @@ class PostController extends Controller
       ]);
       $newpost = new Post;
       $newpost->content = $data['content'];
-      $newpost->author = $data['author_img'];
+      $newpost->author = $data['author'];
       $newpost->content_img = $data['content_img'];
       $newpost->author_img = $data['author_img'];
 
@@ -115,8 +115,16 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+      if(empty($post)) {
+        abort('404');
+      }
+
+        $deleted = $post->delete();
+
+      if ($deleted) {
+        return redirect()->route("posts.index")->with('delete', $post );
+      }
     }
 }
